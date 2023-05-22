c6da8318a56312fead6929a570b54c1aa48b7cb6
mmarquesEE <mateus.marques@ee.ufcg.edu.br>
2023-05-21 23:25:20 -0300

    altered hyperparameters

--- fuzzyCreate.m
+++ fuzzyCreate.m
@@ -6,42 +6,44 @@ d = {'dNO','dES','dWE'};
 fis = mamfis('Name','behaviour');
 
 eGMF = {'N','M','F'};
-fis = addVar(fis,"input",'eG',[0 13],eGMF,[0 3]);
+fis = addVar(fis,"input",'eG',[0 3],eGMF);
 
-alphaGMF = {'SOn','SE','ES','NE','NO','NW','WE','SW','SOp'};
-fis = addVar(fis,"input",'alphaG',[-pi,pi],alphaGMF,[-pi,pi]);
+fis = addInput(fis,[-pi,pi],'Name','alphaG');
+fis = addMF(fis,'alphaG','trapmf',[-2*pi,-pi,-pi/6,0],'Name','ES');
+fis = addMF(fis,'alphaG','trimf',[-pi/6,0,pi/6],'Name','NO');
+fis = addMF(fis,'alphaG','trapmf',[0,pi/6,pi,2*pi],'Name','WE');
 
 eMF = {'S','M','B'};
-fis = addVar(fis,'output','e',[0 3],eMF,[0 5]);
+fis = addVar(fis,'output','e',[0 3],eMF);
 
-alphaMF = {'SOn','SE','ES','NE','NO','NW','WE','SW','SOp'};
-fis = addVar(fis,"output",'alpha',[-pi,pi],alphaMF,[-pi,pi]);
+alphaMF = {'ES','NE','NO','NW','WE'};
+fis = addVar(fis,"output",'alpha',[-pi/2,pi/2],alphaMF);
 
 for i = 1:numel(d)
     fis = addInput(fis,[0 1],'Name',d{i});
-    fis = addMF(fis,d{i},'trapmf',[-0.25,0,0.35,0.5],'Name','S');
-    fis = addMF(fis,d{i},'trimf',[0.25,0.5,0.75],'Name','B');
-    fis = addMF(fis,d{i},'trapmf',[0.5,0.75,1,1.25],'Name','OR');
+    fis = addMF(fis,d{i},'trapmf',[-0.25,0,0.3,0.5],'Name','S');
+    fis = addMF(fis,d{i},'trimf',[0.3,0.5,0.7],'Name','B');
+    fis = addMF(fis,d{i},'trapmf',[0.5,0.7,1,1.25],'Name','OR');
 end
 
 fisGS = addRule(fis,[ ...
-    [(2:3)',zeros(2,1),-ones(2,3),(2:3)',zeros(2,1),ones(2)];...
-    [-ones(9,1),(1:9)',-ones(9,4),(1:9)',ones(9,2)];...
-    [ones(9,1),(1:9)',-ones(9,1),zeros(9,2),ones(9,1),(1:9)',ones(9,2)];...
+    [3,0,0,0,0,3,0,1,1];...
+    [2,0,0,0,0,2,0,1,1];...
+    [1,0,0,0,0,1,0,1,1];...
     ]);
 fisOA = addRule(fis,[ ...
     [zeros(2),(1:2)',zeros(2),(1:2)',zeros(2,1),ones(2)];...
-    [zeros(2),(1:2)',-3*ones(2,1),-ones(2),flip(6:7)',ones(2)];...
-    [zeros(2),(1:2)',-ones(2,1),-3*ones(2,1),zeros(2,1),(3:4)',ones(2)];...
-    [zeros(2),(1:2)',-ones(2),zeros(2,1),(3:4)',ones(2)];...
+    [zeros(2),(1:2)',ones(2,1),zeros(2,1),zeros(2,1),flip(4:5)',ones(2)];...
+    [zeros(2),(1:2)',zeros(2,1),ones(2,1),zeros(2,1),(1:2)',ones(2)];...
     ]);
 fisTR = addRule(fis,[ ...
-    [zeros(5,1),(1:5)',zeros(5,1),-3*ones(5,1),zeros(5,1),2*ones(5,1),5*ones(5,1),ones(5,2)];...
-    [zeros(4,1),(6:9)',zeros(4,1),zeros(4,1),-3*ones(4,1),2*ones(4,1),5*ones(4,1),ones(4,2)];...
+    [0,1,0,-3,0,2,3,1,1];...
+    [0,2,0,0,0,2,3,1,1];...
+    [0,3,0,0,-3,2,3,1,1];...
     ]);
 
 %%
-function fisOut = addVar(fisIn,type,name,range,MF,MFRange)
+function fisOut = addVar(fisIn,type,name,range,MF)
     if type == "input"
         fisOut = addInput(fisIn,range,'Name',name);
     elseif type == "output"
@@ -49,7 +51,7 @@ function fisOut = addVar(fisIn,type,name,range,MF,MFRange)
     end
     
     for i = 1:numel(MF)
-        params = (i - [2,1,0])*diff(MFRange)/(numel(MF)-1) + MFRange(1);
+        params = (i - [2,1,0])*diff(range)/(numel(MF)-1) + range(1);
         fisOut = addMF(fisOut,name,'trimf',params,'Name',MF{i});
     end
 end
--- paretto.m
+++ paretto.m
@@ -3,11 +3,12 @@ function y = paretto(data)
 [m,n] = size(data); idx = 1:m;
 for i=1:n
     idx = find(data(idx,i)==max(data(idx,i)));
+    if numel(idx)==1, break; end
 end
 
 y = max([data(idx,:);zeros(1,n)]);
 if ~any(y)
-    y = [1,1,1];
+    y = [1 1 1];
 end
 
 end
